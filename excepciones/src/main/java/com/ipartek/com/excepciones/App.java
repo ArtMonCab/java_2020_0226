70bf2d4d888ff6656d06423958b9c76887305a24
amont <amontanez@gmail.com>
2020-12-22 11:43:04 +0100

    Ejemplos excepciones

---
+++ excepciones/src/main/java/com/ipartek/com/excepciones/App.java
@@ -0,0 +1,34 @@
+package com.ipartek.com.excepciones;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args) {
+		int a = 5, b = 0, div;
+
+		System.out.println("Inicio");
+		
+		try {
+			String s = null;
+			
+			System.out.println(s.toUpperCase());
+			
+			div = a / b;
+			
+			System.out.println(div);
+		} catch (ArithmeticException e) {
+			System.out.println("No se ha podido efectuar la división: " + e.getMessage());
+		} 
+//		catch (Exception e) {
+//			System.out.println("Error no esperado: " + e.getMessage());
+//		} 
+		finally {
+			System.out.println("Me ejecuto por narices");
+		}
+		
+		System.out.println("Fin");
+	}
+}
---
+++ excepciones/src/main/java/com/ipartek/com/excepciones/PuntoPrueba.java
@@ -0,0 +1,17 @@
+package com.ipartek.com.excepciones;
+
+public class PuntoPrueba {
+	public static void main(String[] args) throws Exception {
+		try {
+			String s = null;
+			
+			System.out.println(s.toUpperCase());
+			
+			Punto p = new Punto(-5, 6);
+		} catch (PuntoException e) {
+			System.out.println("Error al construir el punto: " + e.getMessage());
+		} catch(Exception e) {
+			System.out.println("Error genérico: " + e.getMessage());
+		}
+	}
+}
