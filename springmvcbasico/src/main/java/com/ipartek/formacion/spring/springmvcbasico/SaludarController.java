dd0de303286a8bd5d20dabd5e49bca8e797c0212
Curso JAVA <amontanez@gmail.com>
2021-01-21 09:49:31 +0100

    Parámetro opcional

--- springmvcbasico/src/main/java/com/ipartek/formacion/spring/springmvcbasico/SaludarController.java
+++ springmvcbasico/src/main/java/com/ipartek/formacion/spring/springmvcbasico/SaludarController.java
@@ -3,12 +3,13 @@ package com.ipartek.formacion.spring.springmvcbasico;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SaludarController {
 	
 	@GetMapping("/saludo")
-	public String saludo(String nombre, Model modelo) {
+	public String saludo(@RequestParam(required = false, defaultValue = "Mundo") String nombre, Model modelo) {
 		modelo.addAttribute("nombre",nombre);
 		return "saludo";
 		
