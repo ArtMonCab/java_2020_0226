0ae16facd5a3c1287f2898f2b2101018b793c001
amont <amontanez@gmail.com>
2021-01-25 08:55:02 +0100

    Creamos la entidad Cliente

---
+++ springjdbc/src/main/java/com/ipartek/formacion/spring/springjdbc/entidades/Cliente.java
@@ -0,0 +1,11 @@
+package com.ipartek.formacion.spring.springjdbc.entidades;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+public class Cliente {
+	private Long id;
+	private String nombre, apellidos, cif;
+	private LocalDate fechaNacimiento;
+}
