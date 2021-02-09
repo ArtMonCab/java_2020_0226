a7bb0739fe5b1652afde0e2fa471d7f5e5042bbb
amont <amontanez@gmail.com>
2021-02-09 06:56:25 +0100

    UF2177 Actividad 3

---
+++ UF2177-3/src/main/java/com/ipartek/formacion/spring/UF21773/Uf21773Application.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.UF21773;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uf21773Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uf21773Application.class, args);
+	}
+
+}
---
+++ UF2177-3/src/test/java/com/ipartek/formacion/spring/UF21773/Uf21773ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.UF21773;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uf21773ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
