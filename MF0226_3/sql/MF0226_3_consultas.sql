20544b3bb7e6ea12b97dffac10a72d577be8d4d2
amont <amontanez@gmail.com>
2021-02-10 09:37:24 +0100

    Documentacion y consultas proyecto

--- MF0226_3/sql/MF0226_3_consultas.sql
+++ MF0226_3/sql/MF0226_3_consultas.sql
@@ -1,8 +1,8 @@
 -- SQL listar últimos 5 cursos
-SELECT  c.codigo, c.nombre, c.identificador, c.fInicio, c.fFin, c.nHoras, c.temario, c.activo, cl.nombre as cliente, c.precio, p.nombre as profesor
-FROM curso c, profesor p, cliente cl
-WHERE cl.codigo = c.cliente_codigo
-AND  p.codigo = c.profesor_codigo
+SELECT  c.codigo, c.nombre, c.identificador, c.fInicio, c.fFin, c.nHoras, c.temario, c.activo, 
+	(SELECT nombre FROM cliente where codigo = c.cliente_codigo) as cliente, c.precio,
+    (SELECT nombre FROM profesor where codigo = c.profesor_codigo) as profesor
+FROM curso c
 ORDER By c.fInicio DESC
 LIMIT 5;
 
@@ -14,14 +14,17 @@ LIMIT 5;
 -- SQL detalle curso + número de reseñas
 SELECT  c.nombre, c.identificador, c.nHoras as horas, p.nombre as profesor, (SELECT COUNT(*) FROM resenya
         WHERE curso_codigo = c.codigo) AS numero_resenyas
-FROM curso c, profesor p
-WHERE p.codigo = c.profesor_codigo
+FROM curso c
+LEFT JOIN profesor p
+ON c.profesor_codigo = p.codigo
 GROUP BY c.codigo;
 
+
 -- SQL listado usuarios ordenado por número de reseñas
-SELECT a.nombre, a.apellidos, a.telefono, a.email, (SELECT COUNT(*) FROM resenya
-        WHERE alumno_codigo = a.codigo) AS numero_resenyas 
+SELECT a.nombre, a.apellidos, a.telefono, a.email, count(r.codigo) as numero_resenyas
 FROM alumno a
+LEFT JOIN resenya r
+ON a.codigo = r.alumno_codigo
 GROUP BY a.codigo
 ORDER BY numero_resenyas DESC;
 
