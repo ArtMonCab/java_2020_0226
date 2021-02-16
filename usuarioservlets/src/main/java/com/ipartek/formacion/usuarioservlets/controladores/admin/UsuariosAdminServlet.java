b71078d2b2f60bdcb12615c08100d374f8a18ae7
amont <amontanez@gmail.com>
2021-02-16 14:36:19 +0100

    (Log como variable static final)

---
+++ usuarioservlets/src/main/java/com/ipartek/formacion/usuarioservlets/controladores/admin/UsuariosAdminServlet.java
@@ -0,0 +1,41 @@
+package com.ipartek.formacion.usuarioservlets.controladores.admin;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ipartek.formacion.usuarioservlets.accesodatos.Dao;
+import com.ipartek.formacion.usuarioservlets.accesodatos.UsuarioDaoMySql;
+import com.ipartek.formacion.usuarioservlets.entidades.Usuario;
+
+@WebServlet("/admin/usuarios")
+public class UsuariosAdminServlet extends HttpServlet{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	private static final Logger LOG = Logger.getLogger(UsuariosAdminServlet.class.getName());
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Dao<Usuario> dao = new UsuarioDaoMySql();
+		
+		Iterable<Usuario> usuarios = dao.obtenerTodos();
+		
+		LOG.log(Level.INFO, "Usuarios: {0}", usuarios);
+	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
