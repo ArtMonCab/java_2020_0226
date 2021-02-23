0beccd68c71202af4ec356d9e00430f80cd6cdba
amont <amontanez@gmail.com>
2021-02-23 18:34:28 +0100

    UF1465_3

---
+++ UF1465_3/src/com/ipartek/formacion/uf1465_3/controladores/ControladoresException.java
@@ -0,0 +1,31 @@
+package com.ipartek.formacion.uf1465_3.controladores;
+
+public class ControladoresException extends RuntimeException{
+
+	private static final long serialVersionUID = -3551929526622095725L;
+
+	public ControladoresException() {
+		super();
+	}
+
+	public ControladoresException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ControladoresException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ControladoresException(String message) {
+		super(message);
+	}
+
+	public ControladoresException(Throwable cause) {
+		super(cause);
+	}
+	
+	
+	
+
+}
