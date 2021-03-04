9ee7f9fffb7de95676d69727d9b7085a949a0444
Curso JAVA <amontanez@gmail.com>
2021-03-04 14:53:57 +0100

    Añadir a carrito en sesión con nueva clase Carrito

---
+++ ejemplofinal/src/com/ipartek/formacion/ejemplofinal/controladores/AddCarritoServlet.java
@@ -0,0 +1,51 @@
+package com.ipartek.formacion.ejemplofinal.controladores;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ipartek.formacion.ejemplofinal.entidades.Carrito;
+import com.ipartek.formacion.ejemplofinal.entidades.Producto;
+
+
+@WebServlet("/add-carrito")
+public class AddCarritoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		String id = request.getParameter("id");
+		
+		Producto producto = Config.carritoNegocio.productoPorId(Long.parseLong(id));
+		
+		Carrito carrito = (Carrito) session.getAttribute("carrito");
+		
+		
+		//Todo Mejora la creacion del carrito
+		
+		//Compruebo que el carrito no se ha creado antes.
+		if (carrito == null) {
+			carrito = new Carrito();
+			
+			session.setAttribute("carrito", carrito);
+		}
+		
+		carrito.addProducto(producto, 1);
+		
+		
+		
+		
+		request.getRequestDispatcher(Config.PATH_VISTAS + "carrito.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
