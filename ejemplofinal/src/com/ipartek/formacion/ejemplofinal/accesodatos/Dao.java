427716e3aa7ca2bc36c05c4f543f6e3e6323e27c
Curso JAVA <amontanez@gmail.com>
2021-03-04 14:45:40 +0100

    Acceso a datos con obtenerTodos los productos y fábrica

--- ejemplofinal/src/com/ipartek/formacion/ejemplofinal/accesodatos/Dao.java
+++ ejemplofinal/src/com/ipartek/formacion/ejemplofinal/accesodatos/Dao.java
@@ -2,11 +2,13 @@ package com.ipartek.formacion.ejemplofinal.accesodatos;
 
 import java.util.Set;
 
+import com.ipartek.formacion.ejemplofinal.entidades.Producto;
+
 public interface Dao<T> {
 	default Set<T> obtenerTodos(){
 		throw new AccesoDatosException("NO IMPLEMENTADO");
 	}
-	default T obtenerPorId(Long id){
+	default T obtenerPorId(long id){
 		throw new AccesoDatosException("NO IMPLEMENTADO");
 	}
 	default T insertar(T objeto){
