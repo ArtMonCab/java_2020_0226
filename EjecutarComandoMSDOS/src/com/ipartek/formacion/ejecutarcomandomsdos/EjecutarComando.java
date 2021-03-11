d7897dd1f97a3ffeca79f942a8d3b7ac8e8b5747
Curso JAVA <amontanez@gmail.com>
2021-03-11 08:29:33 +0100

    Ejemplo Backup desde Java

---
+++ EjecutarComandoMSDOS/src/com/ipartek/formacion/ejecutarcomandomsdos/EjecutarComando.java
@@ -0,0 +1,34 @@
+package com.ipartek.formacion.ejecutarcomandomsdos;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+//http://chuwiki.chuidiang.org/index.php?title=Backup_de_MySQL_con_Java
+
+public class EjecutarComando {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		Process p = Runtime.getRuntime()
+				.exec("mysqldump -u root -padmin supermercado");
+
+		try (InputStream is = p.getInputStream(); FileOutputStream fos = new FileOutputStream("backup_pruebas.sql")) {
+			byte[] buffer = new byte[1000];
+
+			int leido;
+			while ((leido = is.read(buffer)) > 0) {
+				fos.write(buffer, 0, leido);
+			}
+		}
+
+		int processComplete = p.waitFor();
+
+		if (processComplete == 0) {
+			System.out.println("Todo correcto");
+		} else {
+			System.out.println("Ha habido algún error");
+		}
+	}
+
+}
