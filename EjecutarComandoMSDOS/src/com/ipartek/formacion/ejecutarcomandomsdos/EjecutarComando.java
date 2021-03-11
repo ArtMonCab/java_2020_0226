ac1fefdd4cb6e03527fe519e6269b17a45921037
Curso JAVA <amontanez@gmail.com>
2021-03-11 16:39:17 +0100

    Ejecutar comando MSDOs, versión mejorada de
    http://chuwiki.chuidiang.org/index.php?title=Backup_de_MySQL_con_Java

--- EjecutarComandoMSDOS/src/com/ipartek/formacion/ejecutarcomandomsdos/EjecutarComando.java
+++ EjecutarComandoMSDOS/src/com/ipartek/formacion/ejecutarcomandomsdos/EjecutarComando.java
@@ -11,7 +11,7 @@ public class EjecutarComando {
 	public static void main(String[] args) throws IOException, InterruptedException {
 
 		Process p = Runtime.getRuntime()
-				.exec("mysqldump -u root -padmin supermercado");
+				.exec("mysqldump -u root  supermercado");
 
 		try (InputStream is = p.getInputStream(); FileOutputStream fos = new FileOutputStream("backup_pruebas.sql")) {
 			byte[] buffer = new byte[1000];
