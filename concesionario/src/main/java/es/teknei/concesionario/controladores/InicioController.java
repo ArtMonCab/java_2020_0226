7ceec25162a0d4a20e044209ca10ae7971713639
Arturo Montañez <amontanez@gmail.com>
2021-05-15 16:40:29 +0200

    Ya accede a la web. Falta acertar con los directorios de la cabecera

--- concesionario/src/main/java/es/teknei/concesionario/controladores/InicioController.java
+++ concesionario/src/main/java/es/teknei/concesionario/controladores/InicioController.java
@@ -3,13 +3,13 @@ package es.teknei.concesionario.controladores;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class InicioController {
 
-	@RequestMapping("/prueba")
-    public String prueba() {
-        
-        return "prueba";
-    }
+	@RequestMapping("/alta")
+	public ModelAndView firstPage() {
+		return new ModelAndView("alta");
+	}
 }
--- concesionario/src/main/webapp/WEB-INF/vistas/alta.jsp
+++ concesionario/src/main/webapp/WEB-INF/vistas/alta.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 
-<%@ include file="/templates/includes/cabecera.jsp" %>
+<%@ include file="/includes/cabecera.jsp" %>
 
 <form  action ="agregar" method="post">
  	<h2>Alta</h2>
@@ -49,4 +49,4 @@
 </form>
   
  
-<%@ include file="/templates/includes/pie.jsp" %>
+<%@ include file="/WEB-INF/vistas/includes/pie.jsp" %>
