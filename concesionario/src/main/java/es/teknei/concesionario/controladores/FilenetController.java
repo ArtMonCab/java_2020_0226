0293be8978a08e8b5929ca5adaa8a565e0bdf73e
Arturo MontaÃ±ez <amontanez@gmail.com>
2021-06-02 23:28:27 +0200

    Primeros intentos con filenet controller y librerias de filenet

--- concesionario/src/main/java/es/teknei/concesionario/controladores/FilenetController.java
+++ concesionario/src/main/java/es/teknei/concesionario/controladores/FilenetController.java
@@ -1,11 +1,31 @@
 package es.teknei.concesionario.controladores;
 
+import javax.security.auth.Subject;
 import javax.swing.text.html.HTMLDocument.Iterator;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.filenet.api.collection.ContentElementList;
+import com.filenet.api.collection.ObjectStoreSet;
+import com.filenet.api.constants.AutoClassify;
+import com.filenet.api.constants.AutoUniqueName;
+import com.filenet.api.constants.CheckinType;
+import com.filenet.api.constants.ClassNames;
+import com.filenet.api.constants.DefineSecurityParentage;
+import com.filenet.api.constants.RefreshMode;
+import com.filenet.api.core.Connection;
+import com.filenet.api.core.Domain;
+import com.filenet.api.core.Factory;
+import com.filenet.api.core.Folder;
+import com.filenet.api.core.ObjectStore;
+import com.filenet.api.core.ReferentialContainmentRelationship;
+import com.filenet.api.util.UserContext;
+import com.filenet.api.core.Document;
 
 import es.teknei.concesionario.entidades.Marca;
 
+@Controller
 public class FilenetController {
 //https://stackoverflow.com/questions/36036311/how-to-store-a-pdf-document-in-filenet
 
@@ -13,13 +33,36 @@ public class FilenetController {
 
 //https://stackoverflow.com/questions/31923654/how-to-update-a-filenet-documents-content-without-changing-the-version
 	
-	public static void insertDocument(Connection conn, String domainName) {
+	// Get object store instance.
+	@GetMapping("/filenet")
+	static void getObjectStoreInstance()    // Example: "ObjectStore1"
+	 {
+		//Parametros conexión
+		String uri = "http://34.234.153.200/wsi/FNCEWS40MTOM";
+		String usuario ="Arturo";
+		String password ="Hola1234$";
+		
+		//Hacer la conexión
+		Connection conn = Factory.Connection.getConnection(uri);
+		Subject subject = UserContext.createSubject(conn, usuario, password, "FIleNetP8WSI");
+		UserContext.get().pushSubject(subject);
+		Domain domain = Factory.Domain.fetchInstance(conn, "Teknei", null);
+	    ObjectStoreSet osColl = domain.get_ObjectStores();
+		String objStoreName = "Teknei";
+	    // Get object store (fetchless instantiation).
+	    ObjectStore ojbStore = Factory.ObjectStore.getInstance(domain, objStoreName);
+
+	    // Show object store name.
+	    ojbStore.refresh();
+	    System.out.println("Object store name: " + ojbStore.get_Name());                    
+	 }
+	/*public static void insertDocument(Connection conn, String domainName) {
 	    // Get domain.
 	    Domain domain = Factory.Domain.fetchInstance(conn, domainName, null);
 	    ObjectStoreSet osColl = domain.get_ObjectStores();
 
 	    // Get each object store.
-	    Iterator iterator = osColl.iterator();
+	    Iterator iterator =  osColl.iterator();
 	    while (iterator.hasNext()) {
 	        // Get next object store.
 	        ObjectStore objStore = (ObjectStore) iterator.next();
@@ -48,10 +91,10 @@ public class FilenetController {
 	        rcr.save(RefreshMode.NO_REFRESH);
 	    }
 	}
-	
-	@GetMapping("/listados")
+	*/
+	/*@GetMapping("/listados")
 	public String subirFilenet(Marca marca) {
-		/*
+		
 		//Parametros conexión
 		String uri = "http://34.234.153.200/wsi/FNCEWS40MTOM";
 		String usuario ="Arturo";
@@ -59,18 +102,19 @@ public class FilenetController {
 		
 		//Hacer la conexión
 		Connection conn = Factory.Connection.getConnection(uri);
-		Subject subject = UserContext.createSubject(conn, username, password, "FIleNetP8WSI");
+		Subject subject = UserContext.createSubject(conn, usuario, password, "FIleNetP8WSI");
 		UserContext.get().pushSubject(subject);
 		
 		try {
 			//Obtener dominio por defecto
-			Domain domain = Factory.Domain.fetchInstance(conn, null, null);
-			System.out.printnl("Domain: " + domain.get_Name());
+			Domain domain = Factory.Domain.fetchInstance(conn, "Teknei", null);
+			System.out.println("Domain: " + domain.get_Name());
 			
 			//Obtener objectos almacenados dominio
 			ObjectStoreSet osSet = domain.get_ObjectStores();
-			ObjectStore store = null
-			Iterator osIter = osSet.iterator.netx();
+			//ObjectStore store = null;
+			ObjectStore objStore = Factory.ObjectStore.fetchInstance(domain, "Teknei", null);
+			Iterator osIter = osSet.Iterator.next();
 			
 			while (osIter.hasNext()) {
 				store = (ObjectStore) osIter.next();
@@ -78,13 +122,13 @@ public class FilenetController {
 			
 			Document doc = Factory.Document.createInstance(store,"Coches");
 			
-			doc.set_MineType("pdf/application");
+			doc.set_MimeType("pdf/application");
 			
 		} finally {
-			USerContext.get().popSubject();
+			UserContext.get().popSubject();
 		}
-		*/
+		
 		return "inicio";
-	}
+	}*/
 
 }
